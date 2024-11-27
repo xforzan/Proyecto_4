8cc851c87b99a2d625ca4a04ffda5095330727cd
xforzan <nakhila2024@gmail.com>
2024-11-27 23:27:31 +0100

    Position

--- Proyecto_4/Data/data.js
+++ Proyecto_4/Data/data.js
@@ -10,19 +10,11 @@ export const data = {
     workExperience: [
       {
         position: "Web Developer",
-        company: "Tech Solutions Inc.",
-        startDate: "2013",
+        company: "Vought",
+        startDate: "MAY 2013",
         endDate: "Present",
         description:
           "As a key member of the development team, I have contributed to various projects, from creating interactive web applications to optimizing database performance.",
-      },
-      {
-        position: "Freelance Web Developer",
-        company: "Self-employed",
-        startDate: "2012",
-        endDate: "2013",
-        description:
-          "During my freelancing period, I collaborated with clients from diverse industries, delivering tailor-made websites that met their unique needs and goals.",
       },
     ],
     skills: [
--- Proyecto_4/Pages/Experience/experience.js
+++ Proyecto_4/Pages/Experience/experience.js
@@ -1,7 +1,6 @@
 import './experience.css';
 
 import { data } from '../../Data/data.js';
-import { button } from '../../Components/Button/button';
 
 export const experience = () => {
     const experience = document.createElement('section');
@@ -12,6 +11,24 @@ export const experience = () => {
     document.body.append(experience);
     const skills = document.createElement('div');
     skills.classList.add('skills');
+
+const ocupation = document.createElement('div');
+ocupation.classList.add('ocupation');
+const job = document.createElement('h3');
+job.innerHTML = `${data.workExperience[0].company}/<span class="position">${data.workExperience[0].position}</span>`;
+ocupation.append(job);
+const date = document.createElement('span');
+date.innerHTML = `${data.workExperience[0].startDate} - ${data.workExperience[0].endDate}`;
+ocupation.append(date);
+
+
+
+const description = document.createElement('p');
+description.classList.add('text');
+description.innerHTML = data.workExperience[0].description;
+experience.append(ocupation);
+experience.append(description);
+
     for (const project of data.skills){
         const skill = document.createElement('div');
         skill.classList.add('skill');
